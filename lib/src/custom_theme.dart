c4d3fd92fb5b88442342ee5ed5230daa27d75671
greymag <greymag@gmail.com>
2020-11-16 14:19:00 +0300

    Set of CustomThemeData's for light and dark themes.

--- lib/src/custom_theme.dart
+++ lib/src/custom_theme.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 import 'package:flutter_custom_theme/flutter_custom_theme.dart';
 
 import 'storage_by_type.dart';
@@ -18,5 +20,34 @@ abstract class ComplexCustomThemeData extends CustomThemeData
     with StorageByTypeMixin {
   ComplexCustomThemeData(List<CustomThemeData> nested) {
     setData(nested.where((item) => item != null), recursive: true);
+  }
+}
+
+/// Set of [CustomThemeData]'s for light and dark themes.
+class CustomThemeDataSet<T extends CustomThemeData> extends CustomThemeData
+    implements StorageByType {
+  final T data;
+  final T dataDark;
+
+  const CustomThemeDataSet({@required this.data, @required this.dataDark})
+      : assert(data != null),
+        assert(dataDark != null);
+
+  @override
+  E get<E>([func]) {
+    if (E == T) {
+      return data as E;
+    }
+
+    if (func != null && data is StorageByType) {
+      return func.call(data as StorageByType);
+    }
+
+    return null;
+  }
+
+  @override
+  Iterable<Type> get types sync* {
+    yield T;
   }
 }
